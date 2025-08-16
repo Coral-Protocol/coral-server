6b6aaf2f638c954e24d2feaa9d9b54d7754306a8
Séafra <Forder>
2025-08-16 21:53:22 +0200

    support runtimes in exported agent section

--- src/main/kotlin/org/coralprotocol/coralserver/orchestrator/UnresolvedAgentExport.kt
+++ src/main/kotlin/org/coralprotocol/coralserver/orchestrator/UnresolvedAgentExport.kt
@@ -1,10 +1,20 @@
 package org.coralprotocol.coralserver.orchestrator
 
+import io.github.oshai.kotlinlogging.KotlinLogging
+import io.ktor.util.toUpperCasePreservingASCIIRules
 import kotlinx.serialization.Serializable
+import org.coralprotocol.coralserver.orchestrator.runtime.RuntimeId
+
+val logger = KotlinLogging.logger {}
 
 @Serializable
 data class UnresolvedAgentExport(
-    val quantity: UInt
+    val quantity: UInt,
+
+    // resolves to List<RuntimeId>, maybe make this type a List<RuntimeId> when ktoml supports lists of enums:
+    // https://github.com/orchestr7/ktoml/issues/340
+    val runtimes: List<String>
+
     // todo: pricing here
 ) {
     fun resolve(name: String, agent: RegistryAgent): AgentExport {
@@ -12,6 +22,28 @@ data class UnresolvedAgentExport(
             throw RegistryException("Cannot export 0 \"$name\" agents")
         }
 
-        return AgentExport(agent, quantity)
+        // Runtimes must be either Executable, Docker or Phala and the runtime must be defined on the imported agent
+        // todo: disallow exporting of Executable runtimes?
+        val validRuntimes = runtimes.mapNotNull {
+            try {
+                val runtimeId = RuntimeId.valueOf(it.toUpperCasePreservingASCIIRules())
+                if (agent.runtime.getById(runtimeId) == null) {
+                    logger.warn { "Runtime \"$it\" is not defined for agent \"$name\"" }
+                    null
+                } else {
+                    runtimeId
+                }
+            }
+            catch (_: IllegalArgumentException) {
+                logger.warn { "Invalid runtime \"$it\" for agent \"$name\"" }
+                null
+            }
+        }
+
+        if (validRuntimes.isEmpty()) {
+            throw RegistryException("Cannot export agent \"$name\" with no runtimes")
+        }
+
+        return AgentExport(agent, validRuntimes, quantity)
     }
 }
