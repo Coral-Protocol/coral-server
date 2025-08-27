46c82ffe73690fd87ac9b8dd35007e3b3a38d9b5
Caelum Forder <caelum119@gmail.com>
2025-08-27 16:06:04 +0100

    wip: test interface improvements

--- src/test/kotlin/org/coralprotocol/coralserver/e2e/RemoteAgentProxyingTest.kt
+++ src/test/kotlin/org/coralprotocol/coralserver/e2e/RemoteAgentProxyingTest.kt
@@ -2,24 +2,49 @@ package org.coralprotocol.coralserver.e2e
 
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.ktor.server.testing.*
+import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 import kotlin.uuid.ExperimentalUuidApi
 
 class RemoteAgentProxyingTest {
+
     @OptIn(ExperimentalUuidApi::class)
     @Test
-    fun testRemoteAgentProxying(): Unit {
+    fun testRemoteAgentProxying(): Unit = runBlocking {
+        // Just to test proxying
         shouldNotThrowAny {
-            testApplication {
-                val exportingServer = TestCoralServer(port = 14391u, devmode = true).apply { setup() }
-                application {
-                    exportingServer.
+            val exportingServer = TestCoralServer(port = 14391u, devmode = true).apply { setup() }
+            val importingServer = TestCoralServer(port = 14392u, devmode = true).apply { setup() }
+
+            importingServer.sessionManager.getOrCreateSession("test", "aaa", "aaa", null)
+            val agent = someKoogAgent()
+            val response = agent.run("blahblahblah") // Step 1
+            println("Agent response: $response")
+            val response2 = agent.run("blahblahblah") // Step 1
+            // Create an agent connecting to the exporting server
+            // Create an agent connecting to the importing server
+            // Get them to exchange a code
+        }
+    }
+
+    private fun createAndConnectAgent() {
+
+    }
+
+    @Test
+    fun testRemoteAgentOrchestration() {
+        testApplication {
+            val exportingServer = TestCoralServer(port = 14391u, devmode = true).apply { setup() }
+            val importingServer = TestCoralServer(port = 14392u, devmode = true).apply { setup() }
+
+            application {
+                with(exportingServer.server!!) {
+                    coralSeverModule()
                 }
-                val importingServer = TestCoralServer(port = 14392u, devmode = true).apply { setup() }
-
-                importingServer.sessionManager.getOrCreateSession("test", "aaa", "aaa", null)
-                importingServer
-
+                // Get remote agent on importing server
+                // Create session post to importing server
+                //    with 2 agents, one local, one remote
+                // mock the execution of the agents and confirm they get executed
             }
         }
     }
