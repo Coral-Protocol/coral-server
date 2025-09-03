b52fefd3620457fc820682ad4468851b5d046d60
Séafra <Forder>
2025-09-03 08:29:01 +0200

    removed unused models

--- src/main/kotlin/org/coralprotocol/coralserver/models/agent/ClaimAgentsModel.kt
+++
@@ -1,9 +0,0 @@
-package org.coralprotocol.coralserver.models.agent
-
-import kotlinx.serialization.Serializable
-import org.coralprotocol.coralserver.agent.remote.RemoteGraphAgentRequest
-
-@Serializable
-data class ClaimAgentsModel(
-    val agents: List<RemoteGraphAgentRequest>
-)
