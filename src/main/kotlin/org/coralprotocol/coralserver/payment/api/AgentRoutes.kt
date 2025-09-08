eb6d8727cb0288e9754be533aadfbbc0de26b0f3
Séafra <Forder>
2025-09-08 18:30:25 +0200

    fix bad imports

--- src/main/kotlin/org/coralprotocol/coralserver/payment/api/AgentRoutes.kt
+++ src/main/kotlin/org/coralprotocol/coralserver/payment/api/AgentRoutes.kt
@@ -1,15 +1,14 @@
 package org.coralprotocol.coralserver.payment.api
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.github.smiley4.ktoropenapi.resources.post
 import io.ktor.http.*
 import io.ktor.resources.*
-import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import mu.KotlinLogging
 import org.coralprotocol.coralserver.payment.config.PaymentServerConfig
-import org.coralprotocol.coralserver.payment.models.*
+import org.coralprotocol.coralserver.payment.models.AvailabilityCheckRequest
 import org.coralprotocol.coralserver.payment.orchestration.SimpleAgentHandler
 import org.coralprotocol.coralserver.payment.utils.ErrorHandling.respondError
 
