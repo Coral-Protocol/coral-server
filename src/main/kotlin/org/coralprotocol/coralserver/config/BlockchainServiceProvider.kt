48a35d570fb801b412fc2243bff478614d3a03d0
Caelum Forder <caelum119@gmail.com>
2025-09-18 20:32:38 -0400

    feat: Generate keys flag

--- src/main/kotlin/org/coralprotocol/coralserver/config/BlockchainServiceProvider.kt
+++ src/main/kotlin/org/coralprotocol/coralserver/config/BlockchainServiceProvider.kt
@@ -3,6 +3,7 @@ package org.coralprotocol.coralserver.config
 import mu.KotlinLogging
 import org.coralprotocol.payment.blockchain.BlockchainService
 import org.coralprotocol.payment.blockchain.BlockchainServiceImpl
+import org.coralprotocol.payment.blockchain.CrossmintBlockchainService
 import org.coralprotocol.payment.blockchain.models.SignerConfig
 import java.io.File
 import java.nio.file.Path
@@ -20,84 +21,57 @@ suspend fun BlockchainService.Companion.loadFromFile(config: Config): Blockchain
         is Wallet.Crossmint -> {
             val rpcUrl = config.paymentConfig.rpcUrl
 
-            val resolvedKeypairPath = run {
-                if (Path.of(wallet.keypairPath).isAbsolute) {
-                    File(wallet.keypairPath)
-                }
-                else {
-                    // Local to wallet config takes priority
-                    val walletPath = Path.of(config.paymentConfig.walletPath)
-                    if (walletPath.parent != null) {
-                        val keypair = walletPath.parent.resolve(wallet.keypairPath).toFile()
-                        if (keypair.exists()) {
-                            return@run keypair
-                        }
-                    }
-
-                    // If there's an existing working directory keypair, we can use that
-                    val workingDir = File(wallet.keypairPath)
-                    if (workingDir.exists()) {
-                        return@run workingDir
-                    }
-
-                    // Nothing exists, default location priority is wallet-relative then working directory if we can't
-                    // get the wallet relative path
-                    if (walletPath.parent != null) {
-                        walletPath.parent.resolve(wallet.keypairPath).toFile()
-                    }
-                    else {
-                        File(wallet.keypairPath)
-                    }
-                }
-            }
+            val resolvedKeypairPath = getResolvedKeypairFile(wallet, config)
 
             if (!resolvedKeypairPath.exists()) {
-                val signerConfig = SignerConfig.Crossmint(
-                    apiKey = wallet.apiKey,
-                    walletLocator = wallet.locator,
-                    walletAddress = wallet.address,
-                    adminSignerLocator = "dummy",
-                    useStaging = wallet.staging
-                )
-
-                val blockchainService = BlockchainServiceImpl(rpcUrl, "confirmed", signerConfig)
-                val info = blockchainService.getCrossmintDelegatedKeypair(resolvedKeypairPath.toString(), false).getOrThrow()
-
-                if (info.createdNew) {
-                    logger.warn { "A new keypair was created and must be signed!" }
-                    logger.warn { "Sign the keypair here: https://sign.coralprotocol.org/#pubkey=${info.publicKey}"}
-                }
+                return null
             }
 
-            // dummy config
             val signerConfig = SignerConfig.Crossmint(
                 apiKey = wallet.apiKey,
-                walletLocator = wallet.locator,
                 walletAddress = wallet.address,
-                adminSignerLocator = "dummy",
                 useStaging = wallet.staging,
                 deviceKeypairPath = resolvedKeypairPath.toString(),
             )
 
             val blockchainService = BlockchainServiceImpl(rpcUrl, "confirmed", signerConfig)
-            blockchainService.getCrossmintDelegatedKeypair(resolvedKeypairPath.toString(), false).fold(
-                onSuccess = {
-                    logger.info { "Successfully loaded keypair from $resolvedKeypairPath" }
-                    logger.info { "Wallet address: ${wallet.address}" }
-
-                    return@loadFromFile blockchainService
-                },
-                onFailure = {
-                    logger.error(it) { "Failed to load keypair from $resolvedKeypairPath" }
-                    logNoPayments()
-
-                    return@loadFromFile null
-                }
-            )
+            blockchainService
         }
         else -> {
             logNoPayments()
             null
+        }
+    }
+}
+
+fun BlockchainService.Companion.getResolvedKeypairFile(
+    wallet: Wallet.Crossmint,
+    config: Config
+): File = run {
+    if (Path.of(wallet.keypairPath).isAbsolute) {
+        File(wallet.keypairPath)
+    } else {
+        // Local to wallet config takes priority
+        val walletPath = Path.of(config.paymentConfig.walletPath)
+        if (walletPath.parent != null) {
+            val keypair = walletPath.parent.resolve(wallet.keypairPath).toFile()
+            if (keypair.exists()) {
+                return@run keypair
+            }
+        }
+
+        // If there's an existing working directory keypair, we can use that
+        val workingDir = File(wallet.keypairPath)
+        if (workingDir.exists()) {
+            return@run workingDir
+        }
+
+        // Nothing exists, default location priority is wallet-relative then working directory if we can't
+        // get the wallet relative path
+        if (walletPath.parent != null) {
+            walletPath.parent.resolve(wallet.keypairPath).toFile()
+        } else {
+            File(wallet.keypairPath)
         }
     }
 }
