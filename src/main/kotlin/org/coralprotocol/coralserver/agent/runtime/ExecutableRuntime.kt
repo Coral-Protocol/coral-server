4942fb8b085724ecb1b6f26a5f35655bd771093b
Séafra <Forder>
2026-02-07 11:02:06 +0100

    feat: coral-agent.toml relative pathing for executables

--- src/main/kotlin/org/coralprotocol/coralserver/agent/runtime/ExecutableRuntime.kt
+++ src/main/kotlin/org/coralprotocol/coralserver/agent/runtime/ExecutableRuntime.kt
@@ -7,22 +7,34 @@ import kotlinx.serialization.Serializable
 import org.coralprotocol.coralserver.logging.LoggingTag
 import org.coralprotocol.coralserver.logging.LoggingTagIo
 import org.coralprotocol.coralserver.session.SessionAgentExecutionContext
+import kotlin.io.path.exists
 
 @Serializable
 @SerialName("executable")
 data class ExecutableRuntime(
     val path: String,
-    val arguments: List<String>,
+    val arguments: List<String> = listOf(),
     override val transport: AgentRuntimeTransport = DEFAULT_AGENT_RUNTIME_TRANSPORT,
 ) : AgentRuntime {
     override suspend fun execute(
         executionContext: SessionAgentExecutionContext,
         applicationRuntimeContext: ApplicationRuntimeContext
     ) {
+        val path = if (executionContext.path != null) {
+            val relative = executionContext.path.resolve(path)
+            if (relative.exists()) {
+                relative.toString()
+            } else {
+                path
+            }
+        } else {
+            path
+        }
+
         executionContext.logger.info { "Executing \"$path\" with arguments: \"${arguments.joinToString(" ")}\"" }
 
         val result = process(
-            command = arguments.toTypedArray(),
+            command = (listOf(path) + arguments).toTypedArray(),
             directory = executionContext.path?.toFile(),
             stdout = Redirect.Consume {
                 it.collect { line -> executionContext.logger.info(LoggingTag.Io(LoggingTagIo.OUT)) { line } }
