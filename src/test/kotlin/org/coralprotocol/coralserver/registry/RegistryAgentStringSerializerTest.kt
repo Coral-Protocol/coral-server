259e457bcf9ae17fa3373e70bf03ae3555397b21
Séafra <Forder>
2026-02-23 12:00:06 +0100

    test: added tests for file references for option defaults

--- src/test/kotlin/org/coralprotocol/coralserver/registry/RegistryAgentStringSerializerTest.kt
+++ src/test/kotlin/org/coralprotocol/coralserver/registry/RegistryAgentStringSerializerTest.kt
@@ -1,13 +1,19 @@
 package org.coralprotocol.coralserver.registry
 
 import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.equals.shouldBeEqual
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.types.shouldBeInstanceOf
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.util.*
 import io.ktor.utils.io.charsets.isSupported
 import org.coralprotocol.coralserver.CoralTest
 import org.coralprotocol.coralserver.agent.registry.UnresolvedRegistryAgent
+import org.coralprotocol.coralserver.agent.registry.option.AgentOption
 import org.koin.test.inject
 import java.io.File
 import java.util.*
@@ -50,7 +56,7 @@ class RegistryAgentStringSerializerTest : CoralTest({
         agent.readme.shouldBeEqual(uuid)
     }
 
-    test("testDisabledUrlReferences").config(enabled = Charsets.isSupported("UTF-8")) {
+    test("testDisabledUrlReferences") {
         val uuid = UUID.randomUUID().toString()
         serveString(uuid)
 
@@ -75,7 +81,7 @@ class RegistryAgentStringSerializerTest : CoralTest({
         }
     }
 
-    test("testStringFileReferenceUtf8").config(enabled = Charsets.isSupported("UTF-8")) {
+    test("testStringFileReferenceUtf8") {
         val agent = UnresolvedRegistryAgent.resolveFromFile(
             File("src/test/resources/string-file-reference/utf8/coral-agent.toml")
         )
@@ -91,12 +97,47 @@ class RegistryAgentStringSerializerTest : CoralTest({
         agent.readme.shouldBeEqual(File("src/test/resources/string-file-reference/windows-1251/README.MD").readText())
     }
 
-    test("testDisabledFileReferences").config(enabled = Charsets.isSupported("UTF-8")) {
+    test("testDisabledFileReferences") {
         shouldThrow<IllegalStateException> {
             UnresolvedRegistryAgent.resolveFromFile(
                 File("src/test/resources/string-file-reference/utf8/coral-agent.toml"),
                 enableFileReferences = false
             )
         }
+    }
+
+    test("testOptionDefaults") {
+        val agent = UnresolvedRegistryAgent.resolveFromFile(
+            File("src/test/resources/string-file-reference/options/coral-agent.toml")
+        )
+
+        val stringValue = File("src/test/resources/string-file-reference/options/string.txt").readText()
+        val blobValue = File("src/test/resources/string-file-reference/options/blob.txt").readText().encodeBase64()
+
+        agent.options["STRING"].shouldNotBeNull().shouldBeInstanceOf<AgentOption.String>().default.shouldNotBeNull()
+            .shouldBeEqual(stringValue)
+
+        agent.options["STRING_BASE64"].shouldNotBeNull()
+            .shouldBeInstanceOf<AgentOption.String>().default.shouldNotBeNull()
+            .shouldBeEqual(stringValue.encodeBase64())
+
+        agent.options["BLOB"].shouldNotBeNull().shouldBeInstanceOf<AgentOption.Blob>().default.shouldNotBeNull()
+            .shouldBeEqual(blobValue)
+
+        agent.options["STRING_LIST"].shouldNotBeNull()
+            .shouldBeInstanceOf<AgentOption.StringList>().default.shouldNotBeNull()
+            .shouldContainExactly(listOf(stringValue, stringValue))
+
+        agent.options["BLOB_LIST"].shouldNotBeNull()
+            .shouldBeInstanceOf<AgentOption.BlobList>().default.shouldNotBeNull()
+            .shouldBeEqual(listOf(blobValue, blobValue))
+
+        agent.options["STRING_NO_DEFAULT"].shouldNotBeNull()
+            .shouldBeInstanceOf<AgentOption.String>().default.shouldBeNull()
+
+        agent.options["BLOB_NO_DEFAULT"].shouldNotBeNull()
+            .shouldBeInstanceOf<AgentOption.Blob>().default.shouldBeNull()
+
+        println(agent)
     }
 })
