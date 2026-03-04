ddaf94241bc3dd2288d5187a391f1f99c7c0b325
Séafra <Forder>
2026-03-04 13:08:23 +0100

    feat: bump default console version to 0.3.2

--- src/main/kotlin/org/coralprotocol/coralserver/config/ConsoleConfig.kt
+++ src/main/kotlin/org/coralprotocol/coralserver/config/ConsoleConfig.kt
@@ -21,7 +21,7 @@ data class ConsoleConfig(
     /**
      * The version of the console to download, will be appended to [consoleReleaseUrl]
      */
-    val consoleReleaseVersion: String = "0.3.1",
+    val consoleReleaseVersion: String = "0.3.2",
 
     /**
      * The name of the zip bundle in the release
