f849cddbd6e4634aee82c1579c18760d379ac957
Séafra <Forder>
2026-03-27 10:18:38 +0100

    test: prototype runtime tests for mcp servers

--- src/test/kotlin/org/coralprotocol/coralserver/session/PrototypeRuntimeTest.kt
+++ src/test/kotlin/org/coralprotocol/coralserver/session/PrototypeRuntimeTest.kt
@@ -1,26 +1,38 @@
 package org.coralprotocol.coralserver.session
 
-
+import io.kotest.core.NamedTag
 import org.coralprotocol.coralserver.CoralTest
 import org.coralprotocol.coralserver.agent.runtime.prototype.PrototypeModelProvider
 import org.coralprotocol.coralserver.agent.runtime.prototype.PrototypeString
+import org.coralprotocol.coralserver.utils.TestMcpServer
 import org.coralprotocol.coralserver.utils.multiAgentPayloadTest
+import org.coralprotocol.coralserver.utils.runTestServerTest
+import org.koin.core.component.get
 
 class PrototypeRuntimeTest : CoralTest({
-    val openaiApiKey = System.getenv("CORAL_TEST_OPENAI_API_KEY")
-    suspend fun openaiPayloadTest(modelName: String) {
-        multiAgentPayloadTest(
-            PrototypeModelProvider.OpenAI(
-                PrototypeString.Inline(openaiApiKey),
-                PrototypeString.Inline(modelName)
-            )
+    val modelProvider = System.getenv("CORAL_TEST_OPENAI_API_KEY")?.let {
+        PrototypeModelProvider.OpenAI(
+            PrototypeString.Inline(it),
+            PrototypeString.Inline("gpt-4.1-mini")
         )
     }
 
-    test("testMultiAgentPayload").config(
-        enabled = openaiApiKey != null,
-        invocations = 10
-    ) {
-        openaiPayloadTest("gpt-4.1-nano")
+    test("testMultiAgentPayload").config(enabled = modelProvider != null) {
+        multiAgentPayloadTest(modelProvider!!)
+    }
+
+    test("testCustomMcpServerNoAuth").config(enabled = modelProvider != null, tags = setOf(NamedTag("debug"))) {
+        val server = TestMcpServer()
+        runTestServerTest(modelProvider!!, server, server.asPrototypeToolServer(get()))
+    }
+
+    test("testCustomMcpServerUrlAuth").config(enabled = modelProvider != null, tags = setOf(NamedTag("debug"))) {
+        val server = TestMcpServer()
+        runTestServerTest(modelProvider!!, server, server.asPrototypeToolServerParamAuth(get()))
+    }
+
+    test("testCustomMcpServerBearerAuth").config(enabled = modelProvider != null, tags = setOf(NamedTag("debug"))) {
+        val server = TestMcpServer()
+        runTestServerTest(modelProvider!!, server, server.asPrototypeToolServerBearerAuth(get()))
     }
 })
