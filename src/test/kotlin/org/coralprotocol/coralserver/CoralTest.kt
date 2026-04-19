6b0646bae4ae532cc25839951b0c5f90bda51b68
Séafra <Forder>
2026-04-19 11:26:51 +0200

    fix: FileAgentRegistrySource now takes a CoroutineScope explicitly, which is cancelled at the end of tests

--- src/test/kotlin/org/coralprotocol/coralserver/CoralTest.kt
+++ src/test/kotlin/org/coralprotocol/coralserver/CoralTest.kt
@@ -1,6 +1,9 @@
+@file:OptIn(KotestInternal::class)
+
 package org.coralprotocol.coralserver
 
 import dev.eav.tomlkt.Toml
+import io.kotest.common.KotestInternal
 import io.kotest.core.NamedTag
 import io.kotest.core.spec.RootTest
 import io.kotest.core.spec.style.FunSpec
@@ -102,6 +105,7 @@ abstract class CoralTest(body: CoralTest.() -> Unit) : KoinTest, FunSpec(body as
                 test = {
                     val testLogger = Logger(logBufferSize, LoggerFactory.getLogger("CoralTest"))
                     val prodLogger = Logger(logBufferSize, LoggerFactory.getLogger("CoralProd"))
+                    val backgroundJob = Job()
 
                     try {
                         runTestApplication {
@@ -212,7 +216,10 @@ abstract class CoralTest(body: CoralTest.() -> Unit) : KoinTest, FunSpec(body as
                                             )
                                         }
                                         single(named(WEBSOCKET_COROUTINE_SCOPE_NAME)) {
-                                            this@RootTest + Job()
+                                            this@RootTest + backgroundJob
+                                        }
+                                        single(named(AGENT_WATCHER_COROUTINE_SCOPE_NAME)) {
+                                            this@RootTest + backgroundJob
                                         }
                                     }
                                 )
@@ -230,13 +237,14 @@ abstract class CoralTest(body: CoralTest.() -> Unit) : KoinTest, FunSpec(body as
                         }
                     } finally {
                         stopKoin()
+                        backgroundJob.cancel()
                     }
                 },
                 type = test.type,
                 source = test.source,
                 config = test.config,
                 factoryId = test.factoryId,
-                xmethod = test.xmethod
+                xmethod = test.xmethod,
             )
         )
     }
