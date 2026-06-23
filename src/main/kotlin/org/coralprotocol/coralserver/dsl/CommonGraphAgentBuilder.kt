a3bbbfbd97d6661dd2ac1200437a34a5b41919d7
Séafra <Forder>
2026-06-23 09:26:17 +0200

    feat: dsl for GraphAgent options

--- src/main/kotlin/org/coralprotocol/coralserver/dsl/CommonGraphAgentBuilder.kt
+++ src/main/kotlin/org/coralprotocol/coralserver/dsl/CommonGraphAgentBuilder.kt
@@ -5,8 +5,8 @@ import org.coralprotocol.coralserver.agent.graph.plugin.GraphAgentPlugin
 import org.coralprotocol.coralserver.agent.payment.AgentBudgetUnit
 import org.coralprotocol.coralserver.agent.registry.RegistryAgentIdentifier
 import org.coralprotocol.coralserver.agent.registry.option.AgentOptionValue
-import org.coralprotocol.coralserver.agent.registry.option.AgentOptionWithValue
 import org.coralprotocol.coralserver.agent.registry.option.AnyAgentOptionWithValue
+import org.coralprotocol.coralserver.agent.registry.option.PolymorphicAgentOptionValue
 import org.coralprotocol.coralserver.agent.runtime.RuntimeId
 import org.coralprotocol.coralserver.llmproxy.LlmProxiedModel
 import org.coralprotocol.coralserver.x402.X402BudgetedResource
@@ -58,6 +58,131 @@ class GraphAgentBuilder(name: String) : CommonGraphAgentBuilder(name) {
     fun option(key: String, value: AnyAgentOptionWithValue) {
         options[key] = value
         registryAgentBuilder.option(key, value.option)
+    }
+
+    fun stringOption(name: String, value: String, block: StringAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.stringOption(name, block).option.withValue(PolymorphicAgentOptionValue.String(value))
+    }
+
+    fun stringListOption(name: String, value: List<String>, block: StringListAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.stringListOption(name, block).option.withValue(PolymorphicAgentOptionValue.StringList(value))
+    }
+
+    fun blobOption(name: String, value: String, block: BlobAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.blobOption(name, block).option.withValue(PolymorphicAgentOptionValue.Blob(value))
+    }
+
+    fun blobListOption(name: String, value: List<String>, block: BlobListAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.blobListOption(name, block).option.withValue(PolymorphicAgentOptionValue.BlobList(value))
+    }
+
+    fun booleanOption(name: String, value: Boolean, block: BooleanAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.booleanOption(name, block).option.withValue(PolymorphicAgentOptionValue.Boolean(value))
+    }
+
+    fun byteOption(name: String, value: Byte, block: ByteAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.byteOption(name, block).option.withValue(PolymorphicAgentOptionValue.Byte(value))
+    }
+
+    fun byteListOption(name: String, value: List<Byte>, block: ByteListAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.byteListOption(name, block).option.withValue(PolymorphicAgentOptionValue.ByteList(value))
+    }
+
+    fun shortOption(name: String, value: Short, block: ShortAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.shortOption(name, block).option.withValue(PolymorphicAgentOptionValue.Short(value))
+    }
+
+    fun shortListOption(name: String, value: List<Short>, block: ShortListAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.shortListOption(name, block).option.withValue(PolymorphicAgentOptionValue.ShortList(value))
+    }
+
+    fun intOption(name: String, value: Int, block: IntAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.intOption(name, block).option.withValue(PolymorphicAgentOptionValue.Int(value))
+    }
+
+    fun intListOption(name: String, value: List<Int>, block: IntListAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.intListOption(name, block).option.withValue(PolymorphicAgentOptionValue.IntList(value))
+    }
+
+    fun longOption(name: String, value: Long, block: LongAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.longOption(name, block).option.withValue(PolymorphicAgentOptionValue.Long(value))
+    }
+
+    fun longListOption(name: String, value: List<Long>, block: LongListAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.longListOption(name, block).option.withValue(PolymorphicAgentOptionValue.LongList(value))
+    }
+
+    fun unsignedByteOption(name: String, value: UByte, block: UByteAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.unsignedByteOption(name, block).option.withValue(PolymorphicAgentOptionValue.UByte(value))
+    }
+
+    fun unsignedByteListOption(name: String, value: List<UByte>, block: UByteListAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.unsignedByteListOption(name, block).option.withValue(PolymorphicAgentOptionValue.UByteList(value))
+    }
+
+    fun unsignedShortOption(name: String, value: UShort, block: UShortAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.unsignedShortOption(name, block).option.withValue(PolymorphicAgentOptionValue.UShort(value))
+    }
+
+    fun unsignedShortListOption(name: String, value: List<UShort>, block: UShortListAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.unsignedShortListOption(name, block).option.withValue(PolymorphicAgentOptionValue.UShortList(value))
+    }
+
+    fun unsignedIntOption(name: String, value: UInt, block: UIntAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.unsignedIntOption(name, block).option.withValue(PolymorphicAgentOptionValue.UInt(value))
+    }
+
+    fun unsignedIntListOption(name: String, value: List<UInt>, block: UIntListAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.unsignedIntListOption(name, block).option.withValue(PolymorphicAgentOptionValue.UIntList(value))
+    }
+
+    fun unsignedLongOption(name: String, value: ULong, block: ULongAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.unsignedLongOption(name, block).option.withValue(PolymorphicAgentOptionValue.ULong(value.toString()))
+    }
+
+    fun unsignedLongListOption(name: String, value: List<ULong>, block: ULongListAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.unsignedLongListOption(name, block).option.withValue(PolymorphicAgentOptionValue.ULongList(value.map { it.toString() }))
+    }
+
+    fun floatOption(name: String, value: Float, block: FloatAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.floatOption(name, block).option.withValue(PolymorphicAgentOptionValue.Float(value))
+    }
+
+    fun floatListOption(name: String, value: List<Float>, block: FloatListAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.floatListOption(name, block).option.withValue(PolymorphicAgentOptionValue.FloatList(value))
+    }
+
+    fun doubleOption(name: String, value: Double, block: DoubleAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.doubleOption(name, block).option.withValue(PolymorphicAgentOptionValue.Double(value))
+    }
+
+    fun doubleListOption(name: String, value: List<Double>, block: DoubleListAgentOptionBuilder.() -> Unit = {}) {
+        options[name] =
+            registryAgentBuilder.doubleListOption(name, block).option.withValue(PolymorphicAgentOptionValue.DoubleList(value))
     }
 
     fun registryAgent(block: RegistryAgentBuilder.() -> Unit) {
