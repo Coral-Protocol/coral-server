5bba3c38e4a6e81783e93c4760e578a3890f5d5d
Séafra <Forder>
2026-07-03 14:15:10 +0200

    test: increase timeout for cloud models to account for provider delay

--- src/test/kotlin/org/coralprotocol/coralserver/llm/CloudTest.kt
+++ src/test/kotlin/org/coralprotocol/coralserver/llm/CloudTest.kt
@@ -13,68 +13,68 @@ class CloudTest : CoralTest({
     val openAI = cloudProviders?.firstOrNull { it.format == LlmProviderFormat.OpenAI }
     val deepSeek = cloudProviders?.firstOrNull { it.format == LlmProviderFormat.DeepSeek }
 
-    test("testCloudOpenAI[o3-mini]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+    test("testCloudOpenAI[o3-mini]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
         multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "o3-mini")
     }
 
-    test("testCloudOpenAI[o4-mini]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+    test("testCloudOpenAI[o4-mini]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
         multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "o4-mini")
     }
 
-    test("testCloudOpenAI[gpt-4.1]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+    test("testCloudOpenAI[gpt-4.1]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
         multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "gpt-4.1")
     }
 
-    test("testCloudOpenAI[gpt-4.1-mini]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+    test("testCloudOpenAI[gpt-4.1-mini]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
         multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "gpt-4.1-mini")
     }
 
     // nano models fail to perform to benchmark
-//    test("testCloudOpenAI[gpt-4.1-nano]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+//    test("testCloudOpenAI[gpt-4.1-nano]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
 //        multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "gpt-4.1-nano")
 //    }
 
-    test("testCloudOpenAI[gpt-5]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+    test("testCloudOpenAI[gpt-5]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
         multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "gpt-5")
     }
 
-    test("testCloudOpenAI[gpt-5.1]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+    test("testCloudOpenAI[gpt-5.1]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
         multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "gpt-5.1")
     }
 
     // not supported by Koog
-//    test("testCloudOpenAI[gpt-5.1-codex-mini]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+//    test("testCloudOpenAI[gpt-5.1-codex-mini]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
 //        multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "gpt-5.1-codex-mini")
 //    }
 
-    test("testCloudOpenAI[gpt-5.2]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+    test("testCloudOpenAI[gpt-5.2]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
         multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "gpt-5.2")
     }
 
     // requires responses API
-//    test("testCloudOpenAI[gpt-5.2-codex]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+//    test("testCloudOpenAI[gpt-5.2-codex]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
 //        multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "gpt-5.2-codex")
 //    }
 
     // requires responses API
-//    test("testCloudOpenAI[gpt-5.3-codex]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+//    test("testCloudOpenAI[gpt-5.3-codex]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
 //        multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "gpt-5.3-codex")
 //    }
 
-    test("testCloudOpenAI[gpt-5.4]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+    test("testCloudOpenAI[gpt-5.4]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
         multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "gpt-5.4")
     }
 
     // nano models fail to perform to benchmark
-//    test("testCloudOpenAI[gpt-5.4-nano]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+//    test("testCloudOpenAI[gpt-5.4-nano]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
 //        multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "gpt-5.4-nano")
 //    }
 
-    test("testCloudOpenAI[gpt-5.4-mini]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+    test("testCloudOpenAI[gpt-5.4-mini]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
         multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "gpt-5.4-mini")
     }
 
-    test("testCloudOpenAI[gpt-5.5]").config(enabledIf = { openAI != null }, invocationTimeout = 1.minutes) {
+    test("testCloudOpenAI[gpt-5.5]").config(enabledIf = { openAI != null }, invocationTimeout = 3.minutes) {
         multiAgentPayloadTest(configuration = openAI!!, client = PrototypeClient.OPEN_AI, model = "gpt-5.5")
     }
 
